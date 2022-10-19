3d6a98f9df8e540e07e378111079177432de20ca
Jun Shin <69499549+greenthings@users.noreply.github.com>
2022-10-19 13:39:12 +0900

    Container restrict the number of argument

--- First/First/ContentView.swift
+++ First/First/ContentView.swift
@@ -11,29 +11,34 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            
-            Text("매출")
-                .font(.title)
-                .padding()
-            
-            HStack(alignment: .top){
-                Text("1분기 판매")
-                Spacer()
-                
-                VStack(alignment: .trailing, spacing: 15) {
-                    Text("1월")
-                    Text("2월")
-                    Text("3월")
-                }
-                
-                Spacer()
-                VStack(alignment: .trailing, spacing: 15) {
-                    Text("$1,000,000")
-                    Text("$21,000,000")
-                    Text("$233,000,000")
-                }
-                
-                
+            // Group도 Container라서 10개이내로 포함이 가능하다.
+            Group{
+                Text("Vstack Test1")
+                Text("Vstack Test2")
+                Text("Vstack Test3")
+                Text("Vstack Test4")
+                Text("Vstack Test5")
+                Text("Vstack Test6")
+                Text("Vstack Test7")
+                Text("Vstack Test8")
+                Text("Vstack Test9")
+                Text("Vstack Test0")
+                //Text("Vstack Test1")
+                //Text("Vstack Test2")
+            }
+            Group{
+                Text("Vstack Test1")
+                Text("Vstack Test2")
+                Text("Vstack Test3")
+                Text("Vstack Test4")
+                Text("Vstack Test5")
+                Text("Vstack Test6")
+                Text("Vstack Test7")
+                Text("Vstack Test8")
+                Text("Vstack Test9")
+                Text("Vstack Test0")
+                //Text("Vstack Test1")
+                //Text("Vstack Test2")
             }
         }
         .padding()
@@ -43,9 +48,32 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-        .previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro"))    .previewDisplayName("IPhone 14 Pro")
-        ContentView()
-            .previewDisplayName("iPhone 14 Plus")
-            .previewDevice(PreviewDevice(rawValue: "iPhone 14 Plus"))
+            .previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro"))    .previewDisplayName("IPhone 14 Pro")
     }
 }
+
+
+
+//Text("매출")
+//    .font(.title)
+//    .padding()
+//
+//HStack(alignment: .top){
+//    Text("1분기 판매")
+//    Spacer()
+//
+//    VStack(alignment: .trailing, spacing: 15) {
+//        Text("1월")
+//        Text("2월")
+//        Text("3월")
+//    }
+//
+//    Spacer()
+//    VStack(alignment: .trailing, spacing: 15) {
+//        Text("$1,000,000")
+//        Text("$21,000,000")
+//        Text("$233,000,000")
+//    }
+//
+//
+//}
